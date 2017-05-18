8e0f5ece5a5fe83ca6698c38ab41827fbddfc417
Long Tian <tianlong0509@gmail.com>
2017-05-18 21:41:11 +0800

    JavaFx web view debug configuration
    Signed-off-by: ltian <ltian@microsoft.com>

--- src/main/java/my/sample/FxWebView.java
+++ src/main/java/my/sample/FxWebView.java
@@ -22,12 +22,11 @@ public class FxWebView extends Application {
         final WebEngine webEngine = view.getEngine();
         webEngine.setJavaScriptEnabled(true);
         JSObject win = (JSObject) webEngine.executeScript("window");
-        // binding fire bug to button FirebugLite
-        webEngine.executeScript("if (!document.getElementById('FirebugLite')){E = document['createElement' + 'NS'] && document.documentElement.namespaceURI;E = E ? document['createElement' + 'NS'](E, 'script') : document['createElement']('script');E['setAttribute']('id', 'FirebugLite');E['setAttribute']('src', 'https://getfirebug.com/' + 'firebug-lite.js' + '#startOpened');E['setAttribute']('FirebugLite', '4');(document['getElementsByTagName']('head')[0] || document['getElementsByTagName']('body')[0]).appendChild(E);E = new Image;E['setAttribute']('src', 'https://getfirebug.com/' + '#startOpened');}");
+        final String userDir = System.getProperty("user.dir");
+        final String htmlIndexFile = "file:///" + userDir + "/src/main/resources/html/index.html";
 
-        // we can ge it from js code window[key]
         win.setMember("key", "value");
-        webEngine.load("https://www.google.com/ncr");
+        webEngine.load(htmlIndexFile);
         primaryStage.setScene(new Scene(view, 600, 600));
         primaryStage.show();
     }
